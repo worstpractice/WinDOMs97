e6d1e243ee6c54f266047847b85162b4d99779c1
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-10-09 21:29:01 +0200

    reverted automatic activate on StartMenu, because we actually focus StartButton already

--- src/components/taskbar/start-area/start-menu/StartMenu.tsx
+++ src/components/taskbar/start-area/start-menu/StartMenu.tsx
@@ -1,6 +1,4 @@
 import { Spine } from "components/taskbar/start-area/start-menu/Spine";
-import { useActivateOnMount } from "hooks/useActivateOnMount";
-import { useMutableRef } from "hooks/useMutableRef";
 import type { FC } from "react";
 import React from "react";
 import styles from "./StartMenu.module.css";
@@ -8,14 +6,10 @@ import styles from "./StartMenu.module.css";
 type Props = {};
 
 export const StartMenu: FC<Props> = ({ children }) => {
-  const startMenuRef = useMutableRef();
-  useActivateOnMount(startMenuRef);
-
   return (
-    <section className={styles.StartMenu} ref={startMenuRef}>
+    <section className={styles.StartMenu}>
       <Spine />
       <ul className={styles.ContentList}>{children}</ul>
     </section>
   );
 };
-
