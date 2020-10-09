36964a11c14e812032f504304bbd612b9713270e
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-10-09 20:55:02 +0200

    88mph

--- src/components/taskbar/notifications-area/NotificationItem.tsx
+++ src/components/taskbar/notifications-area/NotificationItem.tsx
@@ -1,6 +1,10 @@
-import type { FC } from "react";
+import { useMutableRef } from "hooks/useMutableRef";
+import type { FC, MouseEventHandler } from "react";
 import React from "react";
+import { useStore } from "store";
 import type { Process } from "typings/Process";
+import { moveInFront } from "utils/moveInFront";
+import { onLMB } from "utils/onLMB";
 import styles from "./NotificationItem.module.css";
 
 type Props = {
@@ -8,10 +12,21 @@ type Props = {
 };
 
 export const NotificationItem: FC<Props> = ({ process }) => {
+  const { activate } = useStore();
+  const notificationItemRef = useMutableRef();
+
+  /** We must use capture here because the notificationItem is nested deep inside the taskbar. */
+  const handleActiveCapture: MouseEventHandler = onLMB((e) => {
+    // NOTE: This is required since after the capture phase, the event would bubble and hand control back over to the taskbar (which we don't want).
+    e.stopPropagation();
+    activate(process.windowRef);
+    moveInFront(process.windowRef);
+  });
+
   const { icon, name } = process;
 
   return (
-    <li className={styles.NotificationItem}>
+    <li className={styles.NotificationItem} onMouseDownCapture={handleActiveCapture} ref={notificationItemRef as any}>
       <img alt={name} className={styles.Icon} src={icon} />
     </li>
   );
