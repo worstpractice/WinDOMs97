a1057e9522a89d8ca539df2dd2c91dda35e91f66
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-10-10 00:53:01 +0200

    docs

--- src/components/desktop/Shortcut.tsx
+++ src/components/desktop/Shortcut.tsx
@@ -1,14 +1,13 @@
 import { useDesktopLayoutOnMount } from "hooks/useDesktopLayoutOnMount";
 import { useMutableRef } from "hooks/useMutableRef";
 import { useOnMoveShortcut } from "hooks/useOnMoveShortcut";
-import type { FC } from "react";
+import type { FC, MouseEventHandler } from "react";
 import React from "react";
 import { useStore } from "store";
 import type { Binary } from "typings/Binary";
 import { css } from "utils/css";
 import { handleDragStart } from "utils/handleDragStart";
 import { is } from "utils/is";
-import { onLMB } from "utils/onLMB";
 import styles from "./Shortcut.module.css";
 
 type Props = {
@@ -23,13 +22,14 @@ export const Shortcut: FC<Props> = ({ binary, closeMenus }) => {
   // NOTE: For React reasons, this call depends on `Desktop` (the parent component) calling `useActivateOnMount()` to work properly.
   useDesktopLayoutOnMount(shortcutRef.current);
 
-  const handleActive = onLMB((e) => {
+  /** Wrapping this in `onLMB()` prevents momentary highlighting of the shortcut. The flash provides useful interaction feedback however. */
+  const handleActive: MouseEventHandler = (e) => {
     // NOTE: This makes shortcut selection sticky, which we want.
     e.stopPropagation();
     closeMenus();
     activate(shortcutRef);
     handleMove(e);
-  });
+  };
 
   const handleLaunch = () => {
     executeBinary(binary);
