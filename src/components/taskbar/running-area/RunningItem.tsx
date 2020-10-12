996eb963dfc43144671f9a1688323ed03f822afc
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-10-12 02:50:25 +0200

    continued type safety improvements

--- src/components/taskbar/running-area/RunningItem.tsx
+++ src/components/taskbar/running-area/RunningItem.tsx
@@ -1,3 +1,4 @@
+import { onLMB } from "event-filters/onLMB";
 import { useDomRef } from "hooks/useDomRef";
 import { useKernel } from "kernel";
 import type { FC, MouseEventHandler } from "react";
@@ -15,25 +16,25 @@ type Props = {
 
 export const RunningItem: FC<Props> = ({ closeMenus, process }) => {
   const { activate, activeRef } = useKernel();
-  const runningItemRef = useDomRef();
+  const runningItemRef = useDomRef<HTMLButtonElement>();
 
   // NOTE: This is vital. This is the line where each process is given its very own `runningItem` handle.
   process.runningItemRef = runningItemRef;
 
-  const handleActive: MouseEventHandler = (e) => {
+  const handleActive = onLMB<HTMLButtonElement>((e) => {
     // NOTE: This event should not reach the Taskbar below, or it will become active instead of the window we meant to activate.
     e.stopPropagation();
     closeMenus();
     activate(process.windowRef);
     moveInFront(process.windowRef);
-  };
+  });
 
   const style = isRef(activeRef, process.windowRef) ? css(styles.RunningItem, styles.Active) : styles.RunningItem;
 
   const { icon, name } = process;
 
   return (
-    <button className={style} onMouseDown={handleActive} ref={runningItemRef as any} type="button">
+    <button className={style} onMouseDown={handleActive} ref={runningItemRef} type="button">
       <img alt={name} className={styles.Icon} src={icon} />
       <p className={styles.Title}>{name}</p>
     </button>
