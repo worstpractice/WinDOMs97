56c19bbd0d24497aa78ac4fc82e9b756f4265411
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-10-14 03:38:14 +0200

    bending the language until it groans in pain

--- src/utils/reachDeep.ts
+++ src/utils/reachDeep.ts
@@ -1,13 +1,22 @@
 // This file exists merely as a syntax reminder for myself on what destructuring at an arbitrary depth looks like.
+//
+// Much like Kung-fu, we learn it in the hope that we must never use it.
 
-export const os = {
-  motherboard: {
-    cpu: {
-      manufacturer: "Rolands Chark & IT",
+export const computer = {
+  hardware: {
+    motherboard: {
+      cpu: {
+        manufacturer: {
+          name: "Rolands Chark & IT",
+        },
+      },
     },
   },
 } as const;
 
-export const foo = ({ motherboard: { cpu: manufacturer } }: typeof os) => {
-  console.log(manufacturer);
+type Foo = (example: typeof computer) => void;
+
+// prettier-ignore
+export const foo: Foo = ({ hardware: { motherboard: { cpu: { manufacturer: name } } } }) => {
+  console.log(name);
 };
