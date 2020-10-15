38daebdfb708a57a7d30ec6db29f82e87e3721d8
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-10-15 03:00:00 +0200

    optimized kernel performance -- sounds really cool to write dunnit

--- src/kernel/index.ts
+++ src/kernel/index.ts
@@ -49,19 +49,24 @@ export const useKernel = create<State>(
         ({
           /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
           activate<T extends OsRef<HTMLElement>>({ current }: T) {
-            set(({ activeRef }) => {
+            set((state) => {
+              const { activeRef } = state;
+
               console.groupCollapsed(`${++debugLogCounter}. State Changed `);
               console.debug("FROM:", activeRef.current);
               console.debug("TO:", current);
               console.groupEnd();
 
-              return { activeRef: { current } } as const;
+              activeRef.current = current;
+              return state;
             });
           },
           /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
           endProcess({ pid: targetPid }: Process) {
-            set(({ runningProcesses }) => {
-              const sparedProcesses = runningProcesses.filter(({ pid }) => {
+            set((state) => {
+              const { runningProcesses } = state;
+
+              state.runningProcesses = runningProcesses.filter(({ pid }) => {
                 // We spare every process whose `pid` is NOT the `targetPid`.
                 return pid !== targetPid;
               });
@@ -69,12 +74,12 @@ export const useKernel = create<State>(
               // Just like in C, thou shalt remember to free.
               Pids.free(targetPid);
 
-              return { runningProcesses: sparedProcesses } as const;
+              return state;
             });
           },
           /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
           executeBinary(binary: Binary) {
-            set(({ runningProcesses }) => {
+            set((state) => {
               const spawnedProcess: Process = {
                 ...binary,
 
@@ -89,15 +94,23 @@ export const useKernel = create<State>(
                 windowRef: { current: null },
               } as const;
 
-              return { runningProcesses: [...runningProcesses, spawnedProcess] } as const;
+              const { runningProcesses } = state;
+
+              // TODO: Spend a bunch of time designing some intricate footgun just to avoid creating this extra array on every call.
+              state.runningProcesses = [...runningProcesses, spawnedProcess];
+
+              return state;
             });
           },
           /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
           setLastClickPosition({ x, y }: Position) {
-            set(({ lastClickPosition }) => {
+            set((state) => {
+              const { lastClickPosition } = state;
+
               lastClickPosition.x = x;
               lastClickPosition.y = y;
-              return { lastClickPosition } as const;
+
+              return state;
             });
           },
           /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
