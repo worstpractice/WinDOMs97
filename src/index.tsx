32780a5316ff9eab71bb34551709c23b298d6b95
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-10-18 16:16:22 +0200

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { OS } from "components/OS";
 import React, { StrictMode } from "react";
 import { render } from "react-dom";
 import "styles/reset.module.css";
-import "utils/FlatQueue";
 
 // Disable the default context menu globally
 document.oncontextmenu = () => false;
