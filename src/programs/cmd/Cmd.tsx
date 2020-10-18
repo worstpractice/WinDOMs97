91056548c0358eb452fb5fdc382c677a889bc018
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-10-18 23:15:39 +0200

    cleanup

--- src/programs/cmd/Cmd.tsx
+++ src/programs/cmd/Cmd.tsx
@@ -1,4 +1,3 @@
-import { onRMB } from "event-filters/onRMB";
 import * as React from "react";
 import type { FC } from "typings/FC";
 import styles from "./Cmd.module.css";
@@ -6,12 +5,8 @@ import styles from "./Cmd.module.css";
 type Props = {};
 
 export const Cmd: FC<Props> = () => {
-  const handleContextMenu = onRMB(() => {
-    console.log("I felt that!");
-  });
-
   return (
-    <main className={styles.Cmd} onContextMenu={handleContextMenu}>
+    <main className={styles.Cmd}>
       <p>Microsoft Windows [Version 10.0.19041.508]</p>
       <p>(c) 2020 Microsoft Corporation. All rights reserved.</p>
     </main>
