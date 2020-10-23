7fff5f4898db2967dbd440b587445ecfde1d00e9
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-10-23 04:13:17 +0200

    reduce gap

--- src/views/taskbar/start-area/start-menu/Spine.tsx
+++ src/views/taskbar/start-area/start-menu/Spine.tsx
@@ -1,5 +1,4 @@
 import { Title } from "components/Title";
-import { Words } from "components/Words";
 import { default as React } from "react";
 import type { FC } from "typings/FC";
 import styles from "./Spine.module.css";
