1aa54327d6610ae964c300d3cadc82100f81b1ad
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-12-04 16:23:41 +0100

    Scooby this shouldn't compile O.o

--- src/typings/Mutable.d.ts
+++ src/typings/Mutable.d.ts
@@ -3,5 +3,5 @@ import type { ImmutableBrand } from "typings/phantom-types/brands/ImmutableBrand
 export type Mutable<T> = T extends ImmutableBrand
   ? never
   : {
-      -readonly [K in keyof T]: T[K] extends never ? T & ImmutableBrand : Mutable<T[K]>;
+      -readonly [K in keyof T]: T[K] extends never ? T : Mutable<T[K]>;
     };
